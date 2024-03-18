2ea5eb214c005795360699a88a0904143d97a072
Marianojgm86 <59839138+Marianojgm86@users.noreply.github.com>
2024-03-18 14:58:53 -0600

    reordnamiento de herramienta
    Se reordenan algunos elementos dentro de la herramienta.

--- main.m
+++ main.m
@@ -47,79 +47,9 @@ PATH.temp = genpath('temp');
 
 structfun(@addpath, PATH)
 
-%% Configuración de fechas
-DATES.hist_start = qq(2005, 1);
-DATES.hist_end = qq(2023, 4);
-DATES.pred_start = DATES.hist_end + 1;
-DATES.pred_end = DATES.hist_end + 30;
-DATES.hist_end_ant = qq(2023 ,3);
-DATES.hist_end_estimation = qq(2023,1);
-MODEL.DATES = DATES;
+%% Carga de configuraciones generales del corrimiento base (v0)
+StartConfig;
 
-% tab range for simulations
-tab_range = [MODEL.DATES.hist_end, MODEL.DATES.pred_start:MODEL.DATES.pred_start+3, qq(2025,4), qq(2026,4)];
-
-% tab range for source data (quartely)
-tab_range_source_data = [MODEL.DATES.hist_end-8:MODEL.DATES.hist_end];
-
-% tab range for source data (monthly)
-MODEL.DATES.hist_end_mm = mm(2023, 12);
-MODEL.DATES.hist_start_mm = mm(2005,1);
-tab_range_mm = [MODEL.DATES.hist_end_mm-8:MODEL.DATES.hist_end_mm];
-
-%% Configuración inicial del modelo
-MODEL.mod_file_name = 'SVAR50L.mod';
-MODEL.param_file_name = 'setparam.m';
-
-MODEL.CORR_VER = 'v0';
-
-MODEL.CORR_DATE = '2024-02';
-MODEL.CORR_DATE_ANT = '2023-11';
-
-MODEL.leg_act = 'Febrero 2024';  
-MODEL.leg_ant = 'Noviembre 2023'; 
-
-MODEL.data_file_name = fullfile( ...
-    'data', ...
-    'corrimientos', ...
-    MODEL.CORR_DATE, ...
-    MODEL.CORR_VER, ...
-    'data_corr.csv' ...
-);
-
-MODEL.FULLDATANAME_ACT = fullfile( ...
-    'data', ...
-    'fulldata', ...
-    MODEL.CORR_DATE,...
-    sprintf('fulldata_%s_%s.csv', MODEL.CORR_DATE,MODEL.CORR_VER) ...
-);
-
-MODEL.FULLDATANAME_ANT = fullfile( ...
-    'data', ...
-    'fulldata', ...
-    MODEL.CORR_DATE_ANT,...
-    sprintf('fulldata_%s_%s.csv', MODEL.CORR_DATE_ANT,MODEL.CORR_VER) ...
-);
-
-
-%% Nombre de matriz de coeficientes
-MODEL.COV_MAT_NAME = 'COV_MAT.CSV';
-MODEL.GAMMA_0_NAME = 'GAMMA_0.CSV';
-MODEL.PHI_NAME = 'PHI.CSV';
-
-%%
-% Variables con shock estructural (Estructura Endogena del SVAR
-MODEL.ExoVar = { ...
-                'd4_ln_y_star', ...1
-                'd4_ln_ipei', ...2
-                'i_star', ...3
-                'd4_ln_cpi_nosub',...4    
-                'd4_ln_y', ...5    
-                'd4_ln_cpi_sub', ...6
-                'd4_ln_s',...7
-                'd4_ln_bm', ...8 
-                'i'};
-            
 %% PRE-PROCESSING
 PreProcessing;
 
@@ -142,38 +72,38 @@ MODEL = PostProcessing(MODEL,...
                        'list',pp_list,...
                        'list_niv', list_nivel);
 
-%% plots
-% datos fuente y Preprocesamiento
-PreProcPlots;
-% Simulación
-SimulationPlots;
-% Post Procesamiento
-PostProcPlots;
-%% Otras Gráficas
-% tipo de cambio real
-tc_real(MODEL,...
+%% Gráficas 
+do_graphs = false;
+
+if do_graphs == true
+    % datos fuente y Preprocesamiento
+    PreProcPlots;
+    % Simulación
+    SimulationPlots;
+    % Post Procesamiento
+    PostProcPlots;
+    
+    % Otras Gráficas
+    % Componentes Tipo de cambio real
+    tc_real(MODEL,...
         'corr_ant', MODEL.CORR_DATE_ANT,...
         'tab_range', tab_range,...
         'pred_ant', qq(2023, 3));
+    
+    % Componentes Velocidad de Circulación de la Base Monetaria
+    velocidad_subplot;
+    
+    % Descomposición de choques para variables seleccionadas
+    Desc_shocks
+end
 
-% Velocidad
-velocidad_subplot;
+%% Escenarios alternos
 
-%% Descomposición de choques
+Alterno;
+Contrafactual;
 
-MODEL = SimTools.sim.shd_dsc(MODEL);
-list = [MODEL.ExoVar(4:end), 'd4_ln_cpi', 'd4_ln_z', 'd4_ln_v', 'r'];
-% Rango Completo
-temp_path = fullfile('plots',MODEL.CORR_DATE,MODEL.CORR_VER,'Shock_dec\long');
-SimTools.scripts.plot_shd_dsc(MODEL, 'SavePath', temp_path,...
-                             'Rng', {},...
-                             'Variables',list);
-% Rango corto
-temp_path = fullfile('plots',MODEL.CORR_DATE,MODEL.CORR_VER,'Shock_dec\short');
-SimTools.scripts.plot_shd_dsc(MODEL, 'SavePath', temp_path,...
-                             'Rng', MODEL.DATES.hist_end-20:MODEL.DATES.hist_end+20,...
-                             'Variables',list);                         
-                                                    
+
+return
 %% Presentación
 presentacion;
 
@@ -184,4 +114,5 @@ post_proc = MODEL.PostProc;
 save(fullfile('data', 'fulldata',MODEL.CORR_DATE, sprintf("PreProcessing-%s.mat", MODEL.CORR_DATE)), 'pre_proc');
 save(fullfile('data', 'fulldata',MODEL.CORR_DATE, sprintf("PostProcessing-%s.mat", MODEL.CORR_DATE)), 'post_proc');
 
+save(fullfile('data','fulldata',MODEL.CORR_DATE, sprintf("MODEL-%s.mat", MODEL.CORR_DATE)), 'MODEL');
 
