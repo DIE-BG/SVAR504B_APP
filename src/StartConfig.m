8b5bccb9650ff6336814d8082b08d3f363da5a28
Jorge Orenos <JGOR@banguat.gob.gt>
2024-03-22 16:31:31 -0600

    generación corrimientos

--- src/StartConfig.m
+++ src/StartConfig.m
@@ -5,11 +5,11 @@ MODEL.param_file_name = 'setparam.m';
 %% Configuración del corrimento
 MODEL.CORR_VER = 'v0';
 
-MODEL.CORR_DATE = '2024-02';
-MODEL.CORR_DATE_ANT = '2023-11';
+MODEL.CORR_DATE = '2021-11';
+MODEL.CORR_DATE_ANT = '2021-11';
 
-MODEL.leg_act = 'Febrero 2024';  
-MODEL.leg_ant = 'Noviembre 2023'; 
+MODEL.leg_act = 'Noviembre 2021';  
+MODEL.leg_ant = 'Noviembre 2021'; 
 
 MODEL.data_file_name = fullfile( ...
     'data','corrimientos', MODEL.CORR_DATE, MODEL.CORR_VER, 'data_corr.csv');
@@ -35,21 +35,21 @@ MODEL_ANT = MODEL_ANT.MODEL;
 
 %% Configuración de estructura DATES
 MODEL.DATES.hist_start = qq(2005, 1);
-MODEL.DATES.hist_end = qq(2023, 4);
+MODEL.DATES.hist_end = qq(2021, 3);
 
 MODEL.DATES.pred_start = MODEL.DATES.hist_end + 1;
 MODEL.DATES.pred_end = MODEL.DATES.hist_end + 30;
-MODEL.DATES.hist_end_ant = qq(2023 ,3);
+MODEL.DATES.hist_end_ant = qq(2021, 3);
 MODEL.DATES.hist_end_estimation = qq(2023,1);
 
 % Rango de tablas para gráficos de simulación
-tab_range = [MODEL.DATES.hist_end, MODEL.DATES.pred_start:MODEL.DATES.pred_start+3, qq(2025,4), qq(2026,4)];
+tab_range = [MODEL.DATES.hist_end, MODEL.DATES.pred_start:MODEL.DATES.pred_start+3, qq(2022,4), qq(2023,4)];
 
 % Rango de tablas para gráficos de Pre - procesamiento
 % Trimestral
 tab_range_source_data = MODEL.DATES.hist_end-8:MODEL.DATES.hist_end;
 % Mensual
-MODEL.DATES.hist_end_mm = mm(2023, 12);
+MODEL.DATES.hist_end_mm = mm(2021, 10);
 MODEL.DATES.hist_start_mm = mm(2005,1);
 tab_range_mm = MODEL.DATES.hist_end_mm-8:MODEL.DATES.hist_end_mm;
 
