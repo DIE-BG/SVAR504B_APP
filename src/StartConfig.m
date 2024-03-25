ca410c2a8d840e45642dabd1f7c99de9fd5fd069
Marianojgm86 <59839138+Marianojgm86@users.noreply.github.com>
2024-03-25 16:10:52 -0600

    Update StartConfig.m
    Se re-ordena StartConfig.m para unir en una sección de código los elementos que deben actualizarse en cada corrimiento.

--- src/StartConfig.m
+++ src/StartConfig.m
@@ -5,12 +5,18 @@ MODEL.param_file_name = 'setparam.m';
 %% Configuración del corrimento
 MODEL.CORR_VER = 'v0';
 
-MODEL.CORR_DATE = '2021-11';
-MODEL.CORR_DATE_ANT = '2021-11';
+MODEL.CORR_DATE = '2024-02';
+MODEL.CORR_DATE_ANT = '2023-11';
 
-MODEL.leg_act = 'Noviembre 2021';  
-MODEL.leg_ant = 'Noviembre 2021'; 
+MODEL.leg_act = 'Febrero 2024';  
+MODEL.leg_ant = 'Noviembre 2023'; 
 
+% Fechas de fin de historia
+MODEL.DATES.hist_end_ant = qq(2023, 3);
+MODEL.DATES.hist_end = qq(2023, 4);
+MODEL.DATES.hist_end_mm = mm(2024, 02);
+
+%% Otros elementos y fechas
 MODEL.data_file_name = fullfile( ...
     'data','corrimientos', MODEL.CORR_DATE, MODEL.CORR_VER, 'data_corr.csv');
 
@@ -22,24 +28,11 @@ MODEL.FULLDATANAME_ANT = fullfile( ...
     'data', 'fulldata', MODEL.CORR_DATE_ANT,...
     sprintf("MODEL-%s.mat", MODEL.CORR_DATE_ANT));
 
-%% Nombres de escenarios
-MODEL.esc_names = {'Libre',...
-                   'Alterno',...
-                   'Contrafactual',...
-                   'Combinado'};
-               
-%% Carga de info mes previo
-MODEL_ANT = load(sprintf('MODEL-%s.mat',MODEL.CORR_DATE_ANT));
-MODEL_ANT = MODEL_ANT.MODEL;
+% Configuración de estructura DATES
 
-
-%% Configuración de estructura DATES
 MODEL.DATES.hist_start = qq(2005, 1);
-MODEL.DATES.hist_end = qq(2021, 3);
-
 MODEL.DATES.pred_start = MODEL.DATES.hist_end + 1;
 MODEL.DATES.pred_end = MODEL.DATES.hist_end + 30;
-MODEL.DATES.hist_end_ant = qq(2021, 3);
 MODEL.DATES.hist_end_estimation = qq(2023,1);
 
 % Rango de tablas para gráficos de simulación
@@ -49,10 +42,18 @@ tab_range = [MODEL.DATES.hist_end, MODEL.DATES.pred_start:MODEL.DATES.pred_start
 % Trimestral
 tab_range_source_data = MODEL.DATES.hist_end-8:MODEL.DATES.hist_end;
 % Mensual
-MODEL.DATES.hist_end_mm = mm(2021, 10);
 MODEL.DATES.hist_start_mm = mm(2005,1);
 tab_range_mm = MODEL.DATES.hist_end_mm-8:MODEL.DATES.hist_end_mm;
 
+%% Nombres de escenarios
+MODEL.esc_names = {'Libre',...
+                   'Esc. IPEI',...
+                   'Esc. Tasa',...
+                   'Combinado'};
+               
+%% Carga de info mes previo
+MODEL_ANT = load(sprintf('MODEL-%s.mat',MODEL.CORR_DATE_ANT));
+MODEL_ANT = MODEL_ANT.MODEL;
 
 %% Listas adicionales
 
@@ -68,8 +69,10 @@ MODEL.ExoVar = { ...
                 'd4_ln_bm', ...8 
                 'i'}; %9
 
-% Lista de variables para post-procesamiento
+%% Lista de variables para post-procesamiento
+% Logaritmos desestacionalizados, tendencias y brechas
 pp_list = {'ln_y_star', 'ln_ipei', 'ln_z','ln_s','ln_cpi_sub','ln_ipei_q','ln_y','ln_bm','ln_v'};
+% Niveles desestacinoalizados y tendencias
 list_nivel = {'ln_y','ln_s','ln_bm'};
 
 % Variables y titulos para gráficas de reconstrucción de nivel
